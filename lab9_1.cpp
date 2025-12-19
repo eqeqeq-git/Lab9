670a90cdf20036d0a084698c75b4625e5a8e7715
eqeqeq-git <pakinwakaek@gmail.com>
2025-12-19 15:36:28 +0700

    Lab9

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,10 +3,16 @@
 using namespace std;
 
 int main(){	
+	double loan,interestRate,payment,Total,NewBalance,Interest;
+	int i= 1;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interestRate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> payment;	
 
+	
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -14,17 +20,27 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
+
+	while(NewBalance > 0){
+		Interest = (loan*interestRate/100.0);
+		Total = loan + Interest;
+		if(Total < payment){
+				payment = Total;
+		}
+		NewBalance = Total - payment;
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << Interest;
+	cout << setw(13) << left << Total;
+	cout << setw(13) << left << payment;
+	cout << setw(13) << left << NewBalance;
+	cout << "\n";
+		loan = NewBalance;	
+		i++;
+
+	}
 	return 0;
 }
+
