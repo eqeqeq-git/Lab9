fabba47e322108dfc7cd7dc626258132a28c5f70
eqeqeq-git <pakinwakaek@gmail.com>
2025-12-19 15:35:09 +0700

    Lab9

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -7,9 +7,6 @@ int main(){
 	cout << "Enter interest rate per year (%): ";
 	cout << "Enter amount you can pay per year: ";
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
